e3028b5d5456d54f8ea6bbfbf43dd9e3abb3ff01
Zak1r <94119532+Zak1r@users.noreply.github.com>
2021-12-04 20:51:22 +0600

    end of the class

--- index.py
+++ index.py
@@ -3,4 +3,83 @@
 
 # 1) написать алгоритм
 # 2) написать псевдокод
-# 3) код
+# 3) код 
+# (модуль - absolute value = abs) abs(4-9)
+
+# print({3, 2, 1})
+# print({3, 2, 1, 11})
+# print({12, 3, 2, 1, 22})
+# print({3, 2, 1, 11, 22})
+# print({99, 88, 77, 66})
+
+# def nearest_value(nums, target):
+#     nums = list(nums)
+#     distances_to_target = []
+#     for num in nums:
+#         distances_to_target.append(abs(target - num))
+
+#     print(nums[distances_to_target.index(min(distances_to_target))])
+
+
+
+
+# print(nearest_value({4, 7, 10, 11, 12, 17}, 9))
+
+
+
+# def nearest_value(nums, target):
+#     nums = list(nums)
+#     distances_to_target = []
+#     for num in nums:
+#         distances_to_target.append(abs(target - num))
+
+#     return nums[distances_to_target.index(min(distances_to_target))]
+
+
+# print(nearest_value({4, 7, 10, 11, 12, 17}, 9))
+
+# print(len({1, 2, 3}))
+
+
+
+# def nearest_value(nums, target):
+#     if len(nums) == 0:  print ('Error')
+#     nums = list(nums)
+#     distances_to_target = []
+#     for num in nums:
+#         distances_to_target.append(abs(target - num))
+
+#     return nums[distances_to_target.index(min(distances_to_target))]
+
+
+# print(nearest_value({4, 7, 10, 11, 12, 17}, 9))
+# print(nearest_value({4, 7, 10, 11, 12, 17}, 8))
+# print(nearest_value({4, 7, 10, 9, 11, 12, 17}, 9))
+# print(nearest_value({}, 8))
+
+# print(len({1, 2, 3}))
+
+
+#чтобы получить первое слово до пробела == split разделяет пробелы
+# def first_word(text: str):
+#     # print(text.split('')[0])                   #____ .split разделяет по пробелам
+#     return(text.split('')[0]) 
+
+# first_word('Hello Amalmalik!')
+
+
+# def is_acceptable_password (password):
+#     # if len (password) > 6:
+#     return len(password)> 6
+#     # else:   return True
+
+# print(is_acceptable_password('short passwod'))
+# print(is_acceptable_password('muchlonger'))
+
+
+
+
+                                                        # .count(0) считать количеств
+
+
+
